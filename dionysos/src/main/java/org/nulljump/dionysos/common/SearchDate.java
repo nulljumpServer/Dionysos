8a747511989e8c90de6b4ba3872d6c66899d9dbd
solra5653 <solra1454@gmail.com>
2023-03-26 17:18:55 +0900

    V 0.0.1

---
+++ dionysos/src/main/java/org/nulljump/dionysos/common/SearchDate.java
@@ -0,0 +1,48 @@
+package org.nulljump.dionysos.common;
+
+import java.sql.Date;
+
+import org.springframework.stereotype.Component;
+
+//@Component("searchDate")   //�ڵ� ��� : DI ��� ����
+public class SearchDate implements java.io.Serializable{
+
+	private static final long serialVersionUID = -793570371163009025L;
+	
+	private Date begin;
+	private Date end;
+	
+	public SearchDate(){}
+
+	public SearchDate(Date begin, Date end) {
+		super();
+		this.begin = begin;
+		this.end = end;
+	}
+
+	public Date getBegin() {
+		return begin;
+	}
+
+	public void setBegin(Date begin) {
+		this.begin = begin;
+	}
+
+	public Date getEnd() {
+		return end;
+	}
+
+	public void setEnd(Date end) {
+		this.end = end;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	@Override
+	public String toString() {
+		return "SearchDate [begin=" + begin + ", end=" + end + "]";
+	}
+	
+}
---
+++ dionysos/src/main/webapp/WEB-INF/views/common/main.jsp
@@ -0,0 +1,21 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8" errorPage="error.jsp" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>dionysos</title>
+
+<script type="text/javascript" src="${ pageContext.servletContext.contextPath }/resources/js/jquery-3.6.3.min.js">
+</script>
+
+</head>
+<body>
+<h1 align="center">메인 페이지</h1>
+<hr>
+
+</body>
+</html>
+
+
